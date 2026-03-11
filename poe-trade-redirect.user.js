da6bf165917a4e6d4c2ece3a629f4c941f553047
李承熹 <c3d1997@gmail.com>
2026-03-11 12:24:53 +0800

    first commit

---
+++ poe-trade-redirect.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         POE Trade 台服跳轉
+// @description  自動將 pathofexile.com 的 Trade 搜尋跳轉至 pathofexile.tw
+// @version      1.0
+// @author
+// @match        *://www.pathofexile.com/trade/*
+// @run-at       document-start
+// ==/UserScript==
+
+const url = new URL(location.href);
+url.hostname = 'pathofexile.tw';
+location.replace(url.toString());
